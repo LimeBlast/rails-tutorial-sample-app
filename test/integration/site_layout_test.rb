4d1557e426b1fb20b002fa4f4bfdad1a615c3b47
Daniel Hollands <daniel@limeblast.co.uk>
2014-09-30 15:04:54 +0100

    Converted a bunch of double quotes into singles

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,16 +2,16 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test "layout links" do
+  test 'layout links' do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path
-    assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
+    assert_select 'a[href=?]', root_path
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
   end
   
-  test "sign up page" do
+  test 'sign up page' do
     get signup_path
     assert_template 'users/new'
   end
