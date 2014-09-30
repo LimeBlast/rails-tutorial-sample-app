2273b3ac2af90c7d5023d38ce878cd8fe3a33db5
Daniel Hollands <daniel@limeblast.co.uk>
2014-09-30 15:53:23 +0100

    Added layout tests for logged in member

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,7 +2,11 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test 'layout links' do
+  def setup
+    @user = users(:michael)
+  end
+
+  test 'layout links for guest' do
     get root_path
     assert_template 'static_pages/home'
     assert_select 'a[href=?]', root_path
@@ -10,7 +14,18 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', about_path
     assert_select 'a[href=?]', contact_path
   end
-  
+
+  test 'layout links for member' do
+    log_in_as(@user)
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_select 'a[href=?]', users_path
+    assert_select 'a[href=?]', user_path(@user)
+    assert_select 'a[href=?]', edit_user_path(@user)
+    assert_select 'a[href=?]', logout_path
+  end
+
   test 'sign up page' do
     get signup_path
     assert_template 'users/new'
