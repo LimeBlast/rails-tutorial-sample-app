0032c965387f5b62d4e616635e963f4ea6ac6401
Daniel Hollands <daniel.hollands@intercity-uk.com>
2014-10-29 15:54:37 +0000

    tests and shit

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,15 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
 
 # == Route Map
@@ -24,6 +29,8 @@ end
 #                   login GET    /login(.:format)                        sessions#new
 #                         POST   /login(.:format)                        sessions#create
 #                  logout DELETE /logout(.:format)                       sessions#destroy
+#          following_user GET    /users/:id/following(.:format)          users#following
+#          followers_user GET    /users/:id/followers(.:format)          users#followers
 #                   users GET    /users(.:format)                        users#index
 #                         POST   /users(.:format)                        users#create
 #                new_user GET    /users/new(.:format)                    users#new
