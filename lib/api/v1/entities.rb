302f0f5f7e61a951a4a04be8176750d617b93b41
Daniel Hollands <daniel.hollands@intercity-uk.com>
2014-10-31 16:53:10 +0000

    Fixed it, but I need to fix it some more

--- lib/api/v1/entities.rb
+++ lib/api/v1/entities.rb
@@ -1,24 +1,15 @@
 module API
   module V1
     module Entities
-      class Users < Grape::Entity
-        expose :id
-        expose :name
-        expose :microposts_count
-        expose :activated_at, as: :activated
-        expose :admin
-      end
-
       class User < Grape::Entity
         expose :id
         expose :name
         expose :activated_at, as: :activated
         expose :admin
         expose :microposts_count
-        expose :microposts, using: 'API::V1::Entities::Microposts', as: :microposts
       end
 
-      class Microposts < Grape::Entity
+      class Micropost < Grape::Entity
         expose :id
         expose :content
         expose :created_at
@@ -28,7 +19,7 @@ module API
         expose :id
         expose :content
         expose :created_at
-        expose :user, using: 'API::V1::Entities::Users', as: :user
+        expose :user, using: 'API::V1::Entities::User', as: :user
       end
     end
   end
--- lib/api/v1/users.rb
+++ lib/api/v1/users.rb
@@ -1,25 +1,34 @@
 module API
   module V1
     class Users < Grape::API
-      resource :users do
+      resource :user do
         desc 'Return list of activated users'
         get do
           users = User.where(activated: true).all
-          present users, with: API::V1::Entities::Users
+          present users, with: API::V1::Entities::User
         end
 
-        desc 'Return a user and their microposts.'
         params do
           requires :id, type: Integer, desc: 'User ID.'
         end
+
         route_param :id do
+          desc 'Return a user.'
           get do
-            user = User.includes(:microposts).find(params[:id])
+            user = User.find(params[:id])
             present user, with: API::V1::Entities::User
           end
+
+          desc "Return a user's feed."
           get :feed do
             feed = User.find(params[:id]).feed.includes(:user)
             present feed, with: API::V1::Entities::Feed
+          end
+
+          desc "Return a user's microposts."
+          get :micropost do
+            microposts = User.find(params[:id]).microposts
+            present microposts, with: API::V1::Entities::Micropost
           end
         end
       end
